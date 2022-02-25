9d584ebd9b2e6077f8e686ca5e5261de7e64d34a
florianPat <florian.patruck@gmx.de>
2022-02-25 12:25:34 +0100

    fix(env): Fix parsing env variable in production with newlines

--- src/firebase-admin/firebase-admin.service.ts
+++ src/firebase-admin/firebase-admin.service.ts
@@ -11,7 +11,9 @@ export class FirebaseAdminService {
       credential: admin.credential.cert({
         projectId: configService.get<string>('FIREBASE_PROJECT_ID', ''),
         clientEmail: configService.get<string>('FIREBASE_CLIENT_EMAIL', ''),
-        privateKey: configService.get<string>('FIREBASE_PRIVATE_KEY', ''),
+        privateKey: configService
+          .get<string>('FIREBASE_PRIVATE_KEY', '')
+          .replace(/\\n/g, '\n'),
       }),
       databaseURL: configService.get<string>('FIREBASE_DATABASE_URL', ''),
     });
